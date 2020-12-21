d270525a4612b1420a96600c23d286bc1c93ed4e
Pedro Muñoz <pedromurivas11@gmail.com>
2020-12-21 19:46:12 +0100

    Comentarios

--- Biblioteca/script/script.js
+++ Biblioteca/script/script.js
@@ -1,9 +1,16 @@
+//Cuando carga la página se carga todo lo que haya aqui dentro.
 window.onload = function() {
+    //Cuando se pulsa el si del formulario muestra dos campos mas que están ocultos.
     let si = document.getElementById("si").addEventListener("click", literaturaInfantil);
+    //Cuando pulso el no del formulario oculta los campos y resetea los campos ocultos.
     let no = document.getElementById("no").addEventListener("click", literaturaInfantil);
+    //Llama a la función para crear el formulario
     crearFormulario();
+    //Cuando se cambie la opcion de literatura se muestra en la seccion de la derecha los libros del tipo de literatura seleccionado.
     let select = document.getElementById("literatura").addEventListener("change", seccionDos);
+    //Llama a la función para mostrar el estado de la página (si esta abierta para meter libros o no)
     seccionTres();
+    //Cada 60 segundo ejecuta la función para ver si ha cambiado el estado de la página
     setInterval(seccionTres, 60000);
 }
 
@@ -12,6 +19,7 @@ window.onload = function() {
 //Variable donde se guardan las alertas y se las paso a la ventana
  var textoVentana = "";
 
+ //Creo una clase para poder crear objetos Libro
 class Libro {
     constructor(Literatura, Nombre, Identificador, FechaPublicacion, NumeroCopias, EdadRecomendada, Observaciones, Modificacion){
         this.Literatura = Literatura;
@@ -27,7 +35,7 @@ class Libro {
 
 
 
-
+//En esta función valido todos los campos del formulario, creo el objeto Libro y notifico los errores del formulario en una ventana nueva.
 function crearFormulario(){
     
     //Cosas utiles
@@ -44,7 +52,7 @@ function crearFormulario(){
      let observaciones = document.getElementById("observaciones");
      let todoCorrecto = "";
 
-
+    //Cada vez que pulse el botón ejecuta todo lo que tiene dentro esto.
     let boton = document.getElementById("submit");
     boton.addEventListener("click", event => {
         todoCorrecto = true;
@@ -157,7 +165,7 @@ function crearFormulario(){
 
      //Si son validos creo un objeto y si no, se lo notifico al usuario
      if(todoCorrecto == true){           
-        
+        //Creo el objeto libro
         let libro = {
              Literatura: literatura.value,
              Nombre: nombreLibro.value,
@@ -169,19 +177,21 @@ function crearFormulario(){
              Modificacion: new Date()
          }         
 
-
+        //Si el arraylibros esta vacío se le mete el libro creado
         if(ArrayLibros.length == 0){
             ArrayLibros.push(libro);
         }else{
+            // Meto en el array IdObjetos el identificador del libro que haya introducido
             for (let index = 0; index < ArrayLibros.length; index++) {             
                 IdObjetos.push(ArrayLibros[index].Identificador);
             }
-        
-
+        //Si el identificador del libro que introducimos esta incluido entra aquí
             if (IdObjetos.includes(identificador.value)) {
                 
                 identificadorAuxiliar = IdObjetos.indexOf(identificador.value);
+                //Si metemos un libro con un identificador ya existente le modifica el número de copias sumando las que ya había con las que estamos introduciendo
                 ArrayLibros[identificadorAuxiliar].NumeroCopias = parseInt(ArrayLibros[identificadorAuxiliar].NumeroCopias) + parseInt(numeroCopias.value);
+                //Metemos la fecha de la modificación
                 ArrayLibros[identificadorAuxiliar].Modificacion = new Date();
             }else{
                 ArrayLibros.push(libro);
@@ -190,29 +200,32 @@ function crearFormulario(){
         
      }else{
         
+        //Si los campos de formulario no son validos llama a la función crearVentana. Más abajo se desarrollará más.
         crearVentana();
         
      }
-
-     console.log(ArrayLibros);
 
     } );
 
 }
 
+//Función para mostrar los campos ocultos si seleccionas literatura infantil o ocultar esos campos y resetearlos.
 function literaturaInfantil(){
    
     let visibilidad;
-
+    
     if(document.getElementById("si").checked){
+        //Se ponen visible los dos campos que estaban ocultos
         visibilidad = document.getElementById("invisible").className = "visible";
+        //Añade a esos campos el atributo required
         document.getElementById("edadR").setAttribute("required", "");
         document.getElementById("observaciones").setAttribute("required", "");
     }else if(document.getElementById("no").checked){
+        //Se ponen invisibles los dos campos
         visibilidad = document.getElementById("invisible").className = "invisible";
         document.getElementById("observaciones").removeAttribute("required");
         document.getElementById("edadR").removeAttribute("required");
-
+        //Se resetean los dos campos invisibles
         document.getElementById("edadR").value = "";
         document.getElementById("observaciones").value = "";
     }
@@ -224,29 +237,30 @@ function literaturaInfantil(){
 //lo cumple visualiza en el div lo que se necesite.
 function seccionDos(){
 
-
+    //Recoge el select
     let desplegable = document.getElementById("literatura");
+    //Recoge el div 
     let informacion = document.getElementById("seccionDos");
 
+    //Pone en blanco el div
     informacion.innerHTML = "";
+    //Te dice que tipo de literatura has seleccionado
     informacion.innerHTML = "Los libros de tipo "+desplegable.value;
+
 
    for (let index = 0; index < ArrayLibros.length; index++) {
        if (desplegable.value == ArrayLibros[index].Literatura) {
-
+        //Pone en el div el nombre de los libros que pertenezcan al tipo de literatura que hayas seleccionado y si pasas el ratón por encima del nombre te dice el número de ejemplares.
         informacion.innerHTML += "<div class='span'><p>"+ArrayLibros[index].Nombre+"</p><span class=tooltiptext>Hay disponibles "+ArrayLibros[index].NumeroCopias+" ejemplar/es.</span></div><br>";
         
        }
-
        if (desplegable.value == "vacio") {
-        informacion.innerHTML += "<div class='span'><p>"+ArrayLibros[index].Nombre+"</p><span class=tooltiptext>Hola</span></div><br>";
-           
+        informacion.innerHTML += "<div class='span'><p>"+ArrayLibros[index].Nombre+"</p><span class=tooltiptext>Hola</span></div><br>";           
        }
-   }
-
-   
+   }   
 }
 
+//Función que pone el estado de la página
 function seccionTres(){
 
     let seccion = document.getElementById("seccionTres");
@@ -275,12 +289,12 @@ function seccionTres(){
 
 }
 
+//Función que devuelve textoVentana
 function recuperarAlertas(){
-    console.log(textoVentana);
     return textoVentana;
 }
 
-
+//Función para abrir una ventana emergente
 function crearVentana(){
     window.open("./paginaVentana.html", "pop-up", "width=500 height=300");
 
--- Biblioteca/script/ventana.js
+++ Biblioteca/script/ventana.js
@@ -1,5 +1,8 @@
 window.onload = function(){
+    //Con el opener accedes al otro js
     let ventana = window.opener;
+    //Recoges el body
     let cuerpo = document.getElementsByTagName("body");
+    //Llamas a la función recuperarAlertas para que ponga en la ventana emergente todos los errores del formulario
     cuerpo[0].innerHTML = ventana.recuperarAlertas();
 }
